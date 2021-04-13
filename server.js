04671a180bc55c667cde5d46b4f609e19211ef33
cariny <perezcariny@gmail.com>
2021-04-12 19:32:16 -0700

    Removing file

--- server.js
+++ server.js
@@ -15,16 +15,7 @@ app.get('/', (req, res) => {
     res.send('Home route')
 })
 
-// app.get('/weather', async (req, res) => {
-//     try {
-//         // console.log(req, res);
-//         // const weather = await `https://api.openweathermap.org/data/2.5/weather?q=oakland&appid=46728db19194c163f1a4da6a95f18554&units=imperial`;
-//         res.send('Weather route');
-//         // res.json
-//     } catch (error) {
-//         console.error(error);
-//     }
-// })
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
@@ -34,5 +25,3 @@ app.listen(port, () => {
 
 // Define routes 
 app.use('/api/weather', weather)
-
-//https://api.openweathermap.org/data/2.5/weather?q=oakland&appid=46728db19194c163f1a4da6a95f18554&units=imperial
