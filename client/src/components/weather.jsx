2ee661d1f924762b30252d745984607c6956a1e5
cariny <perezcariny@gmail.com>
2021-04-14 00:09:25 -0700

    Adding a homepage

--- client/src/components/weather.jsx
+++ client/src/components/weather.jsx
@@ -1,11 +1,15 @@
 import react, { useEffect } from 'react';
 import './weather.styles.scss';
+// get our fontawesome imports
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import plant from '../assets/plant.png'
 
 const Weather = ({ weatherData }) => {
     return (
         <div className='weather-container'>
-            <h1>Smart Plant</h1>
+            <h1>Track your vitamin D intake</h1>
+            <h3>0% of 600 IU GOAL </h3>
             {weatherData &&
                 <div className='weather'>
                     <div className='weather-data'>
@@ -19,7 +23,17 @@ const Weather = ({ weatherData }) => {
                             <p>Description: {weatherData.weather[0].description}</p>
                         </div>
                     </div>
-                    <img src={plant} alt='plant' className='plant' />
+
+                    <svg>
+
+                        <FontAwesomeIcon icon={faCircleNotch}>
+                        </FontAwesomeIcon>
+                        <text x="80" y="125">Total Daily
+                        </text>
+                        <text x="80" y="155">Vitamin D Intake
+                        </text>
+                    </svg>
+                    {/* <img src={plant} alt='plant' className='plant' /> */}
                 </div>
             }
         </div>
