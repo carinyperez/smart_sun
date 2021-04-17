fce5cc3e4d9843f3e08f77c7bbe9d23bb72f45b0
Jenna Toff <shamanlvr@gmail.com>
2021-04-17 01:47:31 -0700

    set up routing from homepage to go to dashboard on sign-in

--- client/src/pages/homepage/homepage.jsx
+++ client/src/pages/homepage/homepage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
             <h1>Welcome to Smart Sun</h1>
             <p>Let's start your journey to a healthy life now</p>
             <Link to="/onboarding"><button>Sign Up</button></Link>
-            <p>Have an account? <Link to="/login">Sign in</Link></p>
+            <p>Have an account? <Link to="/dashboard">Sign in</Link></p>
             </div>
         </div>
     )
