5a5ee6fb3b3a22dc53262a67a2884281bd40307c
cariny <perezcariny@gmail.com>
2021-04-21 11:40:34 -0700

    Prepare for deployment

--- server.js
+++ server.js
@@ -8,20 +8,21 @@ const weather = require('./routes/api/weather');
 const axios = require('axios');
 const jsonfile = require('jsonfile');
 config = jsonfile.readFileSync(__dirname + '/config/config.json');
-const users = require('./routes/users')
+const path = require('path');
+const users = require('./routes/users');
 
-const {login, createUser} = require('./controllers/users');
+const { login, createUser } = require('./controllers/users');
 const mongoose = require('mongoose');
 const auth = require('./middleware/auth');
-const { login, createUser } = require('./controllers/users')
 
 const mongoDB = 'mongodb://localhost:27017/smartSun';
-mongoose.connect(mongoDB,{
+const db = mongoose.connect(mongoDB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 })
+console.log(db);
 // middlewares 
 app.use(express.json({ extended: false }))
 app.use(express.urlencoded({ extended: false }))
@@ -32,6 +33,13 @@ app.get('/', (req, res) => {
     res.send('Home route')
 })
 
+// serve static assets in production 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
     console.log(`Server running on port ${port}`)
